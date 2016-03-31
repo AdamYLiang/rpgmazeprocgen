c4aa6aa9c79d40d348ebb90e43c985a1ac190a39
unknown <ayl292@nyu.edu>
2016-03-31 16:47:25 -0400

    added restart button, TODO:wallmaker, slime counter

--- Assets/Scripts/gameRestart.cs
+++ Assets/Scripts/gameRestart.cs
@@ -4,16 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class gameRestart : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.R)){
+	void ToggleLoad () {
 			SceneManager.LoadScene (0);
-		}
-	
 	}
+
 }
